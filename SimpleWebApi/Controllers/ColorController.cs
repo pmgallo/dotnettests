d3218e2d2503b419c35e2d68d62124a4044bf429
Pablo Gallo <pablo.gallo@evertecinc.com>
2021-12-05 16:11:49 -0300

    Version inicial de la SimpleWebApi

---
+++ SimpleWebApi/Controllers/ColorController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Drawing;
+
+namespace SimpleWebApi.Controllers
+{
+    [ApiController]    
+    public class ColorController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/[controller]/[action]")]
+        public async Task<string> GetRandomColor()
+        {
+            Random rnd = new Random();
+            Color randomColor = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+            var s = $"{randomColor.R.ToString().PadLeft(3, '0')}{randomColor.G.ToString().PadLeft(3, '0')}{randomColor.B.ToString().PadLeft(3, '0')}";
+            await Task.Delay(1000);
+            return s;
+        }
+    }
+}
